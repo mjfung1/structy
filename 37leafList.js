df04413ea88bc8c6dd33ef73c25ec1f2adee5972
mfung <mjfung1@gmail.com>
2021-07-07 00:03:45 -0400

    37

---
+++ 37leafList.js
@@ -0,0 +1,41 @@
+// -------------------------------------- //
+// time:  O(n);
+// space: O(n);
+const leafList = (root) => {
+  // todo
+  const output = [];
+
+  _leafList(root, output);
+
+  return output;
+};
+
+const _leafList = (node, output) => {
+  if (!node) return;
+  if (!node.left && !node.right) output.push(node.val);
+
+  _leafList(node.left, output);
+  _leafList(node.right, output);
+};
+
+// -------------------------------------- //
+
+// time:  O(n);
+// space: O(n);
+const leafList = (root) => {
+  // todo
+  if (!root) return [];
+  const leaves = [];
+  const stack = [root];
+
+  while (stack.length) {
+    let node = stack.pop();
+
+    if (!node.left && !node.right) leaves.push(node.val);
+
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+
+  return leaves;
+};
