3d01caed95f2c5e3f328f9f53ff155ab662085f1
mfung <mjfung1@gmail.com>
2021-07-06 22:24:11 -0400

    34

--- 34allTreePaths.js
+++ 34allTreePaths.js
@@ -1,4 +1,5 @@
 
+// HELPER RECURSION
 
 const allTreePaths = (root) => {
   // todo
@@ -18,4 +19,29 @@ const allTreePaths = (root) => {
   dfs(root, []);
 
   return output;
+};
+
+
+// REGULAR RECURSION. MIND FUCKING!!
+
+const allTreePaths = (root) => {
+  // todo
+  // if no root, there are no paths; so no subarrs
+  if (!root) return [];
+  // if only one node - this will be the path;
+  if (!root.left && !root.right) return [[root.val]];
+
+  const paths = [];
+
+  let leftSubPaths = allTreePaths(root.left);
+  for (let subPath of leftSubPaths) {
+    paths.push([root.val, ...subPath]);
+  }
+
+  let rightSubPaths = allTreePaths(root.right);
+  for (let subPath of rightSubPaths) {
+    paths.push([root.val, ...subPath]);
+  }
+
+  return paths;
 };
