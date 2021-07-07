04eefbabc48ef6bbb30f8f850cc4de896674c63b
mfung <mjfung1@gmail.com>
2021-07-06 23:36:45 -0400

    added more

--- 35treeLevels.js
+++ 35treeLevels.js
@@ -18,14 +18,34 @@ const treeLevels = (root) => {
   return levels;
 };
 
+
+// ------------------------------------------- //
+
+const treeLevels = (root) => {
+  // todo
+  if (!root) return [];
+  const queue = [[root, 0]];
+  const levels = [];
+
+  while (queue.length) {
+    const [root, level] = queue.shift();
+
+    if (!levels[level]) levels[level] = [];
+    levels[level].push(root.val);
+
+    if (root.left) queue.push([root.left, level + 1]);
+    if (root.right) queue.push([root.right, level + 1]);
+  }
+
+  return levels;
+};
+
 // ------------------------------------------- //
 
 const treeLevels = (root) => {
   // todo
   const levels = [];
-  
   _treeLevels(root, levels, 0);
-  
   return levels;
 };
 
@@ -41,3 +61,23 @@ const _treeLevels = (root, levels, level) => {
   _treeLevels(root.left, levels, level+1);
   _treeLevels(root.right, levels, level+1);
 }
+
+// ------------------------------------------- //
+
+const treeLevels = (root) => {
+  // todo
+  const levels = [];
+
+  function dfs(root, level) {
+    if (!root) return;
+
+    if (!levels[level]) levels[level] = [];
+    levels[level].push(root.val);
+
+    dfs(root.left, level + 1);
+    dfs(root.right, level + 1);
+  }
+
+  dfs(root, 0);
+  return levels;
+};
