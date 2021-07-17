c8df4d19f956bf2eb42cf454f6fa73d90dea0b90
mfung <mjfung1@gmail.com>
2021-07-16 23:40:29 -0400

    68

---
+++ 68nestingScore.js
@@ -0,0 +1,29 @@
+
+
+
+// n = length of string
+// Time: O(n)
+// Space: O(n)
+
+const nestingScore = (str) => {
+  // todo
+  const stack = [0];
+
+  for (let bracket of str) {
+    if (bracket === "[") {
+      stack.push(0);
+    } else {
+      let popped = stack.pop();
+
+      // means direct bracket
+      if (popped === 0) {
+        stack[stack.length - 1] += 1;
+      } else {
+        let multiplier = 2 * popped;
+        stack[stack.length - 1] += multiplier;
+      }
+    }
+  }
+  return stack[0];
+};
+
