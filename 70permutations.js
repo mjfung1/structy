5271a1d23aa27b6f4ce398cbb6a5f3962ce69284
mfung <mjfung1@gmail.com>
2021-07-17 23:51:39 -0400

    70

--- 70permutations.js
+++ 70permutations.js
@@ -1,4 +1,35 @@
 
+// n = length of items array
+// time:  ~O(n!)
+// space: ~O(n!)
+
+const permutations = (items) => {
+  // todo
+  // empty array -> a nested array is the only possible permutation
+  if (!items.length) return [[]];
+
+  let first = items[0];
+  let remaining = items.slice(1);
+
+  // just think you are calling with base case, an empty array [].
+  // what will it return? ->  Ans: [[]];
+  let perms = permutations(remaining);
+
+  const fullPermutations = [];
+
+  for (let perm of perms) {
+    // putting first element in every position of perm;
+    for (let i = 0; i <= perm.length; i++) {
+      fullPermutations.push([...perm.slice(0, i), first, ...perm.slice(i)]);
+    }
+  }
+
+  return fullPermutations;
+};
+
+
+
+// ---------------------------------------- //
 
 // helper recursion
 // struggled - almost got it
