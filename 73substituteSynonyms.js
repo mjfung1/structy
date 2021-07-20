9505b7105b074b8add66acedefd010ccac64c611
mfung <mjfung1@gmail.com>
2021-07-20 01:19:35 -0400

    refactored

--- 73substituteSynonyms.js
+++ 73substituteSynonyms.js
@@ -16,11 +16,10 @@ const generate = (words, synonyms) => {
 
   let firstWord = words[0];
   let remainder = words.slice(1);
+  const subArrays = generate(remainder, synonyms);
 
   if (firstWord in synonyms) {
     const result = [];
-
-    const subArrays = generate(remainder, synonyms);
     for (const word of synonyms[firstWord]) {
       // why spread operators.
       // bc we are iterating many times. dont want to mutate arrays.
@@ -28,7 +27,38 @@ const generate = (words, synonyms) => {
     }
     return result;
   } else {
-    const subArrays = generate(remainder, synonyms);
     return subArrays.map((subArray) => [firstWord, ...subArray]);
   }
+};
+
+// --------------------------------------------------- //
+// refactored
+
+const substituteSynonyms = (sentence, synonyms) => {
+  // todo
+  const words = sentence.split(" ");
+  const arrays = generate(words, synonyms);
+  return arrays.map((array) => array.join(" "));
+};
+
+const generate = (words, synonyms) => {
+  if (words.length === 0) return [[]];
+
+  let firstWord = words[0];
+  let remainderWords = words.slice(1);
+  let subArrays = generate(remainderWords, synonyms);
+
+  let result = [];
+  if (firstWord in synonyms) {
+    for (let synonym of synonyms[firstWord]) {
+      for (let subArray of subArrays) {
+        result.push([synonym, ...subArray]);
+      }
+    }
+  } else {
+    for (let subArray of subArrays) {
+      result.push([firstWord, ...subArray]);
+    }
+  }
+  return result;
 };
