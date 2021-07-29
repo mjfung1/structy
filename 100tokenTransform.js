3bbe7402b959009a8ef225865e963652664b9edf
Miguel Fung <mjfung1@gmail.com>
2021-07-29 00:52:57 -0400

    token replace

---
+++ 100tokenTransform.js
@@ -0,0 +1,35 @@
+
+// n = length of longest string of value
+// m = # of unique tokens
+// time:  ~O(n^m)
+// space: ~O(n^m)
+
+const tokenTransform = (s, tokens) => {
+  // todo
+  // if (!s.length) return '';
+  const output = [];
+  
+  let i = 0;
+  let j = 1;
+  
+  while (i < s.length) {
+    if (s[i] !== '$') {
+      output.push(s[i]);
+      i++;
+      j++;
+    } else if (s[j] !== '$') {
+      j+=1;
+    } else {
+      let key = s.slice(i, j+1);
+      let value = tokens[key];
+      // passing in a smaller subset of word s
+      let evaluatedValue = tokenTransform(value, tokens);
+      tokens[key] = evaluatedValue;
+      output.push(evaluatedValue);
+      
+      i=j+1;
+      j=i+1;
+    }
+  }
+  return output.join('');
+};
