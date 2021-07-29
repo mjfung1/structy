1725ee0ad94cc0fb3c12d99b92f58c8cbe089cdf
Miguel Fung <mjfung1@gmail.com>
2021-07-29 00:58:17 -0400

    24

---
+++ 24depthFirstValues.js
@@ -0,0 +1,58 @@
+
+
+
+
+const depthFirstValues = (root) => {
+  // todo
+  const output = [];
+  
+  function dfs(node) {
+    if (!node) return;
+    
+    output.push(node.val);
+    dfs(node.left)
+    dfs(node.right)
+  }
+  
+  dfs(root);
+  return output;
+};
+
+
+// --------------------------------------- //
+// VERY COOL IMPLEMENTATION
+
+const depthFirstValues = (root) => {
+  // todo
+  if (!root) return [];
+  let leftValues = depthFirstValues(root.left);
+  let rightValues = depthFirstValues(root.right);
+  return [root.val, ...leftValues, ...rightValues];
+};
+
+
+// ----------------------------------------- //
+
+const depthFirstValues = (root) => {
+  // todo
+  if (!root) return [];
+  
+  const stack = [ root ];
+  const output = [];
+  
+  while (stack.length) {
+    let node = stack.pop();
+    output.push(node.val);
+    
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+    
+  }
+  
+  return output;
+};
+
+
+
+
+
