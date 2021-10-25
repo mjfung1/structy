3d0f801cddf1da6f15c18dbdcb35ccdce6434dfc
Miguel Fung <mjfung1@gmail.com>
2021-10-25 16:10:24 -0400

    description

--- DynamicProgramming/tabulation/canSum.js
+++ DynamicProgramming/tabulation/canSum.js
@@ -1,3 +1,16 @@
+
+
+// Write a function 'canSum(targetSum, numbers)' that takes in a
+// targetSum and an array of numbers as arguments.Write
+
+// The function should return a boolean indicating whether or not it 
+// is possible to generate the targetSum using numbers from the array.Write
+
+// You may use an element of the array as many times as needed.
+
+// You may assume that all input numbers are nonnegative.
+
+
 // n = amount;
 // time: O(n * m);
 // space: O(n);
