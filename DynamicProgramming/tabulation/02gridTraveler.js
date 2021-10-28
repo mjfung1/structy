811129bcd35d45f34e79716728cc3f435bda0525
Miguel Fung <mjfung1@gmail.com>
2021-10-28 14:23:35 -0400

    changed variables name

--- DynamicProgramming/tabulation/02gridTraveler.js
+++ DynamicProgramming/tabulation/02gridTraveler.js
@@ -15,15 +15,15 @@ const gridTraveler = (m, n) => {
     const table = Array(m + 1).fill().map(() => Array(n + 1).fill(0));
     table[1][1] = 1;
 
-    for (let row = 0; row < table.length; row++) {
-        for (let col = 0; col < table[0].length; col++) {
+    for (let row = 0; row <= m; row++) {
+        for (let col = 0; col <= n; col++) {
             // move right;
-            if (col + 1 < table[0].length) {
+            if (col + 1 <= n) {
                 table[row][col+1] += table[row][col];
             }
 
             // move down;
-            if (row + 1 < table.length) {
+            if (row + 1 <= m) {
                 table[row+1][col] += table[row][col];
             }
         }
