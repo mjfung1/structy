611bb1da28c8edab74dc9af6b49b785017a52e89
Miguel Fung <mjfung1@gmail.com>
2021-10-28 14:07:08 -0400

    tabulation

--- DynamicProgramming/tabulation/02gridTraveler.js
+++ DynamicProgramming/tabulation/02gridTraveler.js
@@ -10,5 +10,29 @@
 // Write a function 'gridTraveler(m,n)' that calculates this.
 
 const gridTraveler = (m, n) => {
+    const table = Array(m + 1).fill().map(() => Array(n + 1).fill(0));
+    table[1][1] = 1;
+
+    for (let row = 0; row < table.length; row++) {
+        for (let col = 0; col < table[0].length; col++) {
+            // move right;
+            if (col + 1 < table[0].length) {
+                table[row][col+1] += table[row][col];
+            }
+
+            // move down;
+            if (row + 1 < table.length) {
+                table[row+1][col] += table[row][col];
+            }
+        }
+    }
     
+    return table[m][n];
 };
+
+
+console.log(gridTraveler(3, 2)) // 3
+console.log(gridTraveler(1, 1)) // 1
+console.log(gridTraveler(2, 3)) // 3
+console.log(gridTraveler(3, 3)) // 6
+console.log(gridTraveler(18, 18)) // 2333606220
