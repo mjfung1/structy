a69c2c3243df8fb88428928aae31a70a69849b76
Miguel Fung <mjfung1@gmail.com>
2021-10-28 15:42:46 -0400

    tabu

--- DynamicProgramming/tabulation/05bestSum.js
+++ DynamicProgramming/tabulation/05bestSum.js
@@ -33,4 +33,7 @@ const bestSum = (targetSum, numbers) => {
     return table[targetSum];
 }
 
-console.log(bestSum(5, [1, 3, 5, 7, 3, 2])) // [3, 2]
+console.log(bestSum(5, [1, 3, 5, 7, 3, 2])) // [5]
+console.log(bestSum(7, [5, 7, 3, 4])) // [7]
+console.log(bestSum(8, [2, 3, 5])) // [3, 5]
+console.log(bestSum(100, [1, 2, 5, 25])) // [25, 25, 25, 25]
