1808322df51bd1e72c7d2d13d068518cad98a4fc
Miguel Fung <mjfung1@gmail.com>
2021-10-29 21:07:36 -0400

    almost had it

--- Practice Runs/Binary Tree/30treePathFinder.js
+++ Practice Runs/Binary Tree/30treePathFinder.js
@@ -16,3 +16,29 @@ const pathFinder = (root, target) => {
   }
   return null;
 };
+
+
+// ----------------------------------
+// better complexity
+const pathFinder = (root, target) => {
+  // todo
+  if (!root) return null;
+  if (root.val === target) return [ root.val ];
+  
+  let values = [] 
+  let left = pathFinder(root.left, target);
+  if (left) {
+    values.push(root.val, ...left);
+  }
+  
+  let right = pathFinder(root.right, target);
+  if (right) {
+    values.push(root.val, ...right);
+  }
+  
+  if (values.length) {
+    return values;
+  } else {
+    return null;
+  }
+};
