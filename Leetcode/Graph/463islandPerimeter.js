a1c465ffd22141b6432ca6dc8661b0078576becb
Miguel Fung <mjfung1@gmail.com>
2021-11-08 21:45:58 -0500

    another attempt

--- Leetcode/Graph/463islandPerimeter.js
+++ Leetcode/Graph/463islandPerimeter.js
@@ -28,3 +28,33 @@ const dfs = (row, col, grid, visited) => {
 
     return perimeter;
 };
+
+
+// -----------------------------------
+var islandPerimeter = function(grid) {
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (grid[row][col] === 1) {
+                return findSize(row, col, grid, new Set());
+            }
+        }
+    }
+};
+
+const findSize = (row, col, grid, visited) => {
+    if (row < 0 || row === grid.length || col < 0 || col === grid[0].length) return 1;
+    if (grid[row][col] === 0) return 1;
+    
+    let key = row + ',' + col;
+    if (visited.has(key)) return 0;
+    visited.add(key);
+    
+    let size = 0;
+    
+    size += findSize(row+1, col, grid, visited);
+    size += findSize(row-1, col, grid, visited);
+    size += findSize(row, col+1, grid, visited);
+    size += findSize(row, col-1, grid, visited);
+    
+    return size;
+};
