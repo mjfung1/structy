74a84a611326719fa3474a556b5b695be45016fa
Miguel Fung <mjfung1@gmail.com>
2022-01-08 22:04:45 -0500

    review2

--- JavascriptRound2/3.LinkedList/28.removeNode.js
+++ JavascriptRound2/3.LinkedList/28.removeNode.js
@@ -7,3 +7,23 @@ const removeNode = (head, targetVal) => {
   head.next = removeNode(head.next, targetVal);
   return head;
 };
+
+
+
+// ---------------------
+const removeNode = (head, targetVal) => {
+  // todo
+  let dummyNode = { next: head };
+  let tail = dummyNode;
+  
+  while (head) {
+    if (head.val === targetVal) {
+      tail.next = head.next;  
+    }
+    
+    tail = tail.next;
+    head = head.next;
+  }
+  
+  return dummyNode.next;
+};
