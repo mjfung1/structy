6cca9b3f3e48a23fd054f46c578875fdc98e1705
Miguel Fung <mjfung1@gmail.com>
2022-01-09 18:03:45 -0500

    review 2 easy peacy

--- JavascriptRound2/9.MixedRecall/104.linkedListCycle.js
+++ JavascriptRound2/9.MixedRecall/104.linkedListCycle.js
@@ -14,3 +14,26 @@ const linkedListCycle = (head) => {
   
   return false;
 };
+
+
+
+
+
+
+
+
+const linkedListCycle = (head) => {
+  // todo
+  let slow = head;
+  let fast = head;
+  let firstIteration = true;
+
+  while (fast && fast.next) {
+    if (!firstIteration && slow === fast) return true;
+    firstIteration = false;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  
+  return false;
+};
