407231fde0453799488c11f78454909dda0744bd
Miguel Fung <mjfung1@gmail.com>
2022-03-08 15:49:37 -0500

    review

---
+++ Leetcode/Array/48rotateImage.js
@@ -0,0 +1,16 @@
+
+// 48. Rotate Image
+
+var rotate = function(matrix) {
+    matrix.reverse();
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < i; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    
+    return matrix;
+};
