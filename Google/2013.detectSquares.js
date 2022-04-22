80f4e32965128fb46275ac6d0f534fd8c7079d25
Miguel Fung <mjfung1@gmail.com>
2022-04-22 11:41:19 -0400

    practice writing classes

--- Google/2013.detectSquares.js
+++ Google/2013.detectSquares.js
@@ -46,3 +46,52 @@ DetectSquares.prototype.count = function(point) {
     
     return count;
 };
+
+
+
+// -----------------------------------------
+// practice writing classes
+
+
+class DetectSquares {
+    constructor() {
+        this.points = {};
+    }
+    
+    add(point) {
+        const [x, y] = point;
+        let key = x + ',' + y;
+
+        if (!(key in this.points)) this.points[key] = 0;
+        this.points[key]++;
+    }
+    
+    count(point) {
+        let count = 0;
+        let [x, y] = point;
+
+        for (let key of Object.keys(this.points)) {
+            let [px, py] = key.split(',');
+            // px = Number(px);
+            // py = Number(py);
+
+            // if diff of (x - px )and (y - py), not valid square
+            // square must have positive area.  
+            // if x == px or y == py that means point on top of each other
+            if ((Math.abs(x - px) !== Math.abs(y - py)) || (x == px) || (y == py)) {
+                continue;
+            }
+
+            let point1 = x + ',' + py;
+            let point2 = px + ',' + y;
+            // if we dont hit continue, that means we found valid diagonal;
+            if ((point1 in this.points) && (point2 in this.points)) {
+                count += this.points[key] * this.points[point1] * this.points[point2];
+            }
+        }
+    
+        return count;
+    }
+    
+    
+}
